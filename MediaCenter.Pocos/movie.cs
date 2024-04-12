7faea1a182ea6cce2d65880690d6fbb5d182bcec
elahe_khavidi <elahe.khavidi@akerna.com>
2024-04-11 21:09:02 -0400

    Remove ignored files

--- MediaCenter.Pocos/movie.cs
+++ MediaCenter.Pocos/movie.cs
@@ -2,11 +2,12 @@ using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
+
 namespace MediaCenter.Pocos
 {
-    public class movies
+     public class Movie
     {
-         [Key]
+        [Key]
         public int Id { get; set; }
         public string Title { get; set; }
         public string Genre { get; set; }
--- MediaCenter.Pocos/user.cs
+++ MediaCenter.Pocos/user.cs
@@ -1,16 +1,18 @@
-using System;
+﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace MediaCenter.Pocos
-{
-    public class users
-    {
+namespace MediaCenter.Pocos{
+
+public class User
+  {
         [Key]
         public int Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
         public string Email { get; set; }
         public string Role { get; set; }
-    
+
     }
 }
+
